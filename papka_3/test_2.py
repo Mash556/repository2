cdee01583f5787e68812411956f22bd0285d28ce
Mash556 <almashnurmatk.gmail.com>
2023-11-10 21:19:23 +0600

    дз OOP

--- papka_3/test_2.py
+++ papka_3/test_2.py
@@ -16,22 +16,6 @@ obj.swim()
 
 
 
-
-class Auto:
-    def ride(self):
-        print('Riding on a ground')
-    
-class Boat:
-    def swim(self):
-        print('Floating on water')
-    
-class Amphibian(Auto, Boat):
-    pass
-
-obj = Amphibian()
-obj.ride()
-obj.swim() 
-
 class ContactList(list):
     def __init__(self, args):
         self.args = args
